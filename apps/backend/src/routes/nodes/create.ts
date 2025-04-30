2db6ac1d85258128d9570b5d4361a6f208678608
TheOldZoom <xabou2010@gmail.com>
2025-04-30 07:40:30 -0600

    cool comment

--- apps/backend/src/routes/nodes/create.ts
+++ apps/backend/src/routes/nodes/create.ts
@@ -3,7 +3,7 @@ import Router from "../../structures/Router";
 const router = new Router();
 
 router.post((req, res) => {
-  // create a node by connecti
+  // create a node by it's ip, port, label, authorization & then add the url to a script that will check if it is up or not (websocket)
 });
 
 export default router;
